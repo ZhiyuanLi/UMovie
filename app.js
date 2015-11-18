1c6396fd8d7632b8c34307e4c98e27df00504029
Zhiyuan Li <zhil@seas.upenn.edu>
2015-11-17 19:38:40 -0500

    mysql connection

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-/**
- * Author: Kaushik Yasaswy
- * Date: Saturday, 26-Sep-15 07:56:18 UTC
- */
-
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host : 'u-moive.cvgkmmzz4oct.us-east-1.rds.amazonaws.com',
+	user : 'awsuser',
+	password : 'ffffffff',
+	port : '3306',
+	database : 'U_Moive'
+});
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -14,52 +17,73 @@ var yourwork = require('./routes/yourworkRoute');
 
 var app = express();
 
+
 console.log('Project UMovie');
+
+connection.connect();
+
+var queryString = 'SELECT * FROM genre';
+ 
+connection.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+ 
+    for (var i in rows) {
+        console.log('Genre: ', rows[i].genre);
+    }
+});
+ 
+connection.end();
+
+console.log('CIS450/550 Homework3');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended : false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-// if you get a request for the sampleResponse page, call the 'displayResponse' function present in the 'sampleRoute' route
+// if you get a request for the sampleResponse page, call the 'displayResponse'
+// function present in the 'sampleRoute' route
 app.get('/sampleResponse', sample.displayResponse);
-// if you qet a request for the yourworkResponse page, call the 'displayResponse' function present in the 'yourworkRoute' route
+// if you qet a request for the yourworkResponse page, call the
+// 'displayResponse' function present in the 'yourworkRoute' route
 app.get('/yourworkResponse', yourwork.displayResponse);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
 
 // development error handler
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message : err.message,
+			error : err
+		});
+	});
 }
 
 // production error handler
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message : err.message,
+		error : {}
+	});
 });
 
 module.exports = app;
