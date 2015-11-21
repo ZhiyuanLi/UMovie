c967288d888f9d5703696122387b152e05bbaa24
Zhiyuan Li <zhil@seas.upenn.edu>
2015-11-20 19:14:47 -0500

    search(movie name+ person name)

--- routes/index.js
+++ routes/index.js
@@ -11,23 +11,56 @@ var connection = mysql.createConnection({
 });
 
 /* get latest movie from mysql database */
-function showLatestMovie(req, res, next) {
-	var latestMovie = 'SELECT name, rating, date, abstraction, poster FROM movie ORDER BY date DESC LIMIT 5';
-	connection.query(latestMovie, function(err, rows, fields) {
-		if (err) {
-			throw err;	
-		} else {
-			res.render('homepage', {
-				latestMovies : rows
-			});
-			//connection.end();
-		}
-	});
+// function showLatestMovie(req, res, next) {
+// var latestMovie = 'SELECT name, rating, date, abstraction, poster FROM movie
+// ORDER BY date DESC LIMIT 5';
+// connection.query(latestMovie, function(err, rows, fields) {
+// if (err) {
+// throw err;
+// } else {
+// res.render('homepage', {
+// latestMovies : rows
+// });
+// // connection.end();
+// }
+// });
+// }
+/* get latest movie from mysql database */
+function showSearchMovie(req, res, next) {
+	if (req.query.search != null) {
+		var searchMovie = 'SELECT m.name as mname, p.name as pname, i_job, rating, date, abstraction, poster FROM movie m inner join involve_in i on m.movie_id = i. i_mid inner join person p on p.personId = i.i_pid WHERE UPPER(m.name) LIKE UPPER('+'"%'+ req.query.search + '%")' + 'OR UPPER(p.name) LIKE UPPER('+'"%'+ req.query.search + '%")';
+		connection.query(searchMovie, function(err, rows, fields) {
+			if (err) {
+				throw err;
+			} else {
+				res.render('homepage', {
+					search_results : rows,
+					latestMovies : null
+					
+				});
+				// connection.end();
+			}
+		});
+	} else {
+		var latestMovie = 'SELECT name, rating, date, abstraction, poster FROM movie ORDER BY date DESC LIMIT 5';
+		connection.query(latestMovie, function(err, rows, fields) {
+			if (err) {
+				throw err;
+			} else {
+				res.render('homepage', {
+					latestMovies : rows,
+					search_results:null
+				});
+				// connection.end();
+			}
+		});
+	}
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	showLatestMovie(req, res, next);
+	// showLatestMovie(req, res, next);
+	showSearchMovie(req, res, next)
 });
 
 router.get('/topRanked', function(req, res, next) {
@@ -41,4 +74,3 @@ router.get('/tagsMovie', function(req, res, next) {
 });
 
 module.exports = router;
-
